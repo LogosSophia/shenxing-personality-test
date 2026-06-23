09406bbcb678cdf8874c4376ab0a65faf1451be1
LogosSophia <guomingtaologos@gmail.com>
2026-06-23 20:03:29 +0800

    Reload v0.7 app module on Streamlit reruns

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """Streamlit entrypoint for 神性论人格王国测评 v0.7 beta."""
+import importlib
 import sys
+
 import backend_v07
 
 sys.modules["backend"] = backend_v07
-import app_v07
+
+if "app_v07" in sys.modules:
+    importlib.reload(sys.modules["app_v07"])
+else:
+    import app_v07
